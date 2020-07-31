0ac2ba1b5409ac2f7570626be3989687afe6946b
Dute607 <709004490@qq.com>
2020-07-31 14:05:52 +0800

    daima

---
+++ 2020-7/2020-7/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  2020-7
+//
+//  Created by edz on 2020/7/31.
+//  Copyright © 2020 edz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
