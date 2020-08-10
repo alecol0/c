fb574c998365a9747d93b76623fbf2fb800093d2
Dute607 <709004490@qq.com>
2020-08-10 20:17:48 +0800

    8-10

---
+++ 2020-8/8-10/练习12-1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  练习12-1
+//
+//  Created by edz on 2020/8/10.
+//  Copyright © 2020 edz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
